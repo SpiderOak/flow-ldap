19ade90fa6f2bc70023a226917ec371cb2bf2090
Lucas Manuel Rodriguez <lucas@spideroak.com>
2016-09-06 13:00:55 -0300

    Split client mode for Windows

--- src/semaphor_ldap.py
+++ src/semaphor_ldap.py
@@ -30,6 +30,43 @@ def run_cli(options):
     cli_obj.run()
 
 
+def add_cli_options(parser):
+    # Add API methods
+    cli_subparsers = parser.add_subparsers(
+        title="Commands", metavar="{API COMMAND FROM THE LIST}")
+    api_gen.add_api_methods(cli_subparsers)
+
+
+def create_generic_arg_parser():
+    parser = argparse.ArgumentParser(
+        description="%s is a server/cli app to enable the use of "
+                    "Semaphor with Customer LDAP credentials." % (
+                        os.path.basename(sys.argv[0])
+                    ),
+    )
+    parser.add_argument("--version", action="version", version=utils.VERSION)
+    # Generic config
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Debug Mode",
+        default=False,
+    )
+    return parser
+
+
+def run_cli_as_default(options):
+    """Run the CmdCli object, to be used in windows
+    so we don't have to use the client prefix every time
+    we use the client mode.
+    """
+    parser = create_generic_arg_parser()
+    add_cli_options(parser)
+    options = parser.parse_args(sys.argv[1:])
+    cli_obj = CmdCli(options)
+    cli_obj.run()
+
+
 def signal_handler(sig, frame):
     """Function to gracefully terminate.
     sys.exit will raise SystemExit that
@@ -53,24 +90,9 @@ def run_server(options, stop_server_event=None):
             server_obj.cleanup()
 
 
-def parse_options(argv):
+def parse_options():
     """Command line options parsing."""
-
-    parser = argparse.ArgumentParser(
-        description="%s is a daemon/cli to enable the use of "
-                    "Semaphor with Customer LDAP credentials." % (
-                        os.path.basename(sys.argv[0])
-                    ),
-    )
-    parser.add_argument("--version", action="version", version=utils.VERSION)
-
-    # Generic config
-    parser.add_argument(
-        "--debug",
-        action="store_true",
-        help="Debug Mode",
-        default=False,
-    )
+    parser = create_generic_arg_parser()
 
     subparsers = parser.add_subparsers(title="Modes")
 
@@ -87,13 +109,10 @@ def parse_options(argv):
     cli_parser = subparsers.add_parser("client", help="Client Mode")
     cli_parser.set_defaults(server_mode=False)
 
-    # Add API methods
-    cli_subparsers = cli_parser.add_subparsers(
-        title="Commands", metavar="{API COMMAND FROM THE LIST}")
-    api_gen.add_api_methods(cli_subparsers)
+    # Add client API methods
+    add_cli_options(cli_parser)
 
-    options = parser.parse_args(argv[1:])
-
+    options = parser.parse_args(sys.argv[1:])
     return options
 
 
@@ -101,7 +120,7 @@ def main():
     """Entry point for the application."""
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-    options = parse_options(sys.argv)
+    options = parse_options()
     if options.server_mode:
         run_server(options)
     else:  # CLI mode
